76d353d579f178fa51810d11e7526b4718061adc
VortXMusic <katsmusicmadness@gmail.com>
2023-03-05 16:05:33 +0530

    Complete Ex 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,24 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...args) {
+    
+    //old solution: no function arguments needed
+    // let arr=arguments[0];
+    // for(let i=1;i<arguments.length;i++)
+    // {
+    //     let arg=arguments[i];
+    //     for(let j=0;j<arr.length;j++)
+    //     {
+    //         if(arr[j]===arg)
+    //             arr.splice(j,1);
+    //     }
+    // }
+    // return arr;
 
+    let result=[];
+    arr.forEach(element => {
+        if(!args.includes(element))
+            result.push(element);
+    });
+    return result;
 };
 
 // Do not edit below this line
