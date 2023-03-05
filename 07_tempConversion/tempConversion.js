d23b079a00a67db25e71e64929f8cb7c7da497e0
VortXMusic <katsmusicmadness@gmail.com>
2023-03-05 16:36:00 +0530

    Complete Ex 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(farenheit) {
+  let celsius=(farenheit-32)*(5/9);
+  celsius = Math.round((celsius+Number.EPSILON)*10)/10;
+  return celsius;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+  let farenheit=celsius*(9/5)+32;
+  farenheit = Math.round((farenheit+Number.EPSILON)*10)/10;
+  return farenheit;
 };
 
 // Do not edit below this line
